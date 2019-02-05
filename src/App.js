0a7030d3e0e901a1f74c5b7a4bd2dc5c9569dbce
Alexandr Nikiforov <kefirnot@gmail.com>
2019-02-06 01:10:33 +0700

    removed excess files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReduxConfig from './reduxConfig';
 import WorkSpace from './components/workspace';
-import './App.css';
 import 'typeface-roboto';
 
 class App extends Component {
